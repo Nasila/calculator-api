a40ee81e4f1a82f9dab2847e6db94050650051fa
Nasila <44721556+Nasila@users.noreply.github.com>
2020-08-29 14:38:58 +0530

    Added api's for calculator

--- src/index.js
+++ src/index.js
@@ -14,6 +14,134 @@ app.use(bodyParser.json())
 // your code goes here
 
 // here
+app.get("/", (__, res) => {
+    res.send("Hello World");
+  });
+
+app.post('/add', (req,res) => {
+    const {num1, num2} = req.body;
+
+    if(typeof num1 === "string" || typeof num2 === "string") {
+        return res.json({
+            status: 'failure',
+            message: 'invalid data types'
+        });
+    }
+
+    else if(num1 > 10000000 || num2 > 10000000) {
+        return res.json({
+            status: 'error',
+            message: 'Overflow'
+        });
+    }
+
+    else {
+        const sum = num1 + num2;
+        return res.json({
+            status: 'success',
+            message: 'the sum of given two number',
+            sum: sum
+            });
+    }
+  
+});
+
+app.post('/sub', (req,res) => {
+    const {num1, num2} = req.body;
+
+    if(typeof num1 === "string" || typeof num2 === "string") {
+        return res.json({
+            status: 'failure',
+            message: 'invalid data types'
+        });
+    }
+
+    else if(num1 > 10000000 && num2 > 10000000) {
+        return res.json({
+            status: 'error',
+            message: 'Overflow'
+        });
+    }
+
+    else {
+        let sum = num1 - num2;
+        return res.json({
+            status: 'success',
+            message: 'the difference of given two number',
+            sum: sum
+            });
+    }
+  
+});
+
+app.post('/multiply', (req,res) => {
+    const {num1, num2} = req.body;
+
+    if(typeof num1 === "string" || typeof num2 === "string") {
+        return res.json({
+            status: 'failure',
+            message: 'invalid data types'
+        });
+    }
+
+    else if(num1 > 10000000 || num2 > 10000000) {
+        return res.json({
+            status: 'error',
+            message: 'Overflow'
+        });
+    }
+
+    else {
+        const sum = num1 * num2;
+        return res.json({
+            status: 'success',
+            message: 'The product of given numbers',
+            sum: sum
+            });
+    }
+  
+});
+
+app.post('/division', (req,res) => {
+    const {num1, num2} = req.body;
+
+    if(typeof num1 === "string" || typeof num2 === "string") {
+        return res.json({
+            status: 'failure',
+            message: 'invalid data types'
+        });
+    }
+
+    else if(num1 > 10000000 || num2 > 10000000) {
+        return res.json({
+            status: 'error',
+            message: 'Overflow'
+        });
+    }
+
+    else {
+        let data = {};
+        if(num2 === 0) {
+            data = {
+                status: 'error',
+                message : 'Cannot divide by zero'
+            }    
+        }
+        else {
+            const sum = num1 / num2;
+            data = {
+                status: 'success',
+                message: 'The division of given numbers',
+                sum: sum
+            } 
+        }
+        
+        return res.json({
+             data
+            });
+    }
+  
+});
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
